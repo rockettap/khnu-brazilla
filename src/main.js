f8bd541d70e9c5146787e5593fa860df5b66ede4
Олександр <67645530+rockettap@users.noreply.github.com>
2024-03-10 14:10:24 +0200

    fixed: burger animation, added: scroll lock

--- src/main.js
+++ src/main.js
@@ -1,2 +1,9 @@
 import "ress";
 import "/scss/main.scss";
+
+const menuButton = document.getElementById("menuButton");
+
+menuButton.addEventListener("click", function () {
+  this.classList.toggle("menu-btn--opened");
+  document.body.classList.toggle("scroll-lock");
+});
