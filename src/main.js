b1f1dd58879cf4bd58c0e2dc6ba6a497bd39fa74
Олександр <67645530+rockettap@users.noreply.github.com>
2024-03-19 21:45:06 +0200

    added: aos, swiper, products section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import "ress";
+
 import "/scss/main.scss";
+
+AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  modules: [Navigation, Pagination],
+  slidesPerView: 1,
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    641: {
+      slidesPerView: 2,
+    },
+    1081: {
+      slidesPerView: 3,
+    },
+  },
+});
 
 const menuBtn = document.getElementById("menu-btn");
 const headerNav = document.getElementById("header-nav");
